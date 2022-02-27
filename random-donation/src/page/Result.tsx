b35e3dc17632ec1ae5413e60cc5b9e0b812ae2ad
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-02-27 16:36:06 +0900

    [Fix] 폴더 구조 정리 및 firebase, router 연동

--- random-donation/src/page/Result.tsx
+++ random-donation/src/page/Result.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const Result = (): JSX.Element => {
-    return(<></>)
-}
+    return (
+        <>
+        </>
+    );
+};
 
 export default Result;
---
+++ random-donation/src/services/fbase.ts
@@ -0,0 +1,25 @@
+// import {} from 'firebase'
+
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig:object = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId:  process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId:  process.env.MESSAGING_SENDER_ID,
+    appId:  process.env.APP_ID,
+    measurementId:  process.env.MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+export const firebaseInstance:any = firebase;
+export const authService:any = firebaseInstance.auth();
+export const dbService:any = firebaseInstance.firestore();
+export const storageService:any = firebaseInstance.storage();
