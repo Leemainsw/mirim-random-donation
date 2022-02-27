fc24d28d776a708f7f921a83edd2938fcefccfba
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-02-27 15:42:15 +0900

    [Fix] page default code setting

--- random-donation/src/page/DonationInfo.tsx
+++ random-donation/src/page/DonationInfo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const DonationInfo = () => {
-
+const DonationInfo = (): JSX.Element => {
+    return(<></>)
 }
 
 
---
+++ random-donation/src/page/Load.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Load = (): JSX.Element => {
+    return(<></>)
+}
+
+export default Load;
