8e2a69dd1fd1411d18b7d28b1eb72029d3840cd2
Areum Yang <s2018s10@e-mirim.hs.kr>
2022-03-01 00:22:05 +0900

    [Add] redux-firebase 추가중

--- random-donation/src/component/DonationInfo/InfoDetail.tsx
+++ random-donation/src/component/DonationInfo/InfoDetail.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
+import UserData from "../../types/type";
+import UserDataService from "../../services/service";
+
+type Props = {};
+
+type State = {
+  users: Array<UserData>;
+};
 
 const InfoDetail = () => {
-
-}
+  return <></>;
+};
 
 export default InfoDetail;
