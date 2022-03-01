9b2273acf5f30a5d2eb1a76c80b8a94368a18d7f
areumsheep <areumsheep@gmail.com>
2022-03-01 23:04:34 +0900

    [Feat] 결과 페이지 반응형 수정

--- random-donation/src/component/Result/ResultText.tsx
+++ random-donation/src/component/Result/ResultText.tsx
@@ -5,131 +5,141 @@ import imageSrc from "../../assets/image.png";
 import { useSelector } from "react-redux";
 
 interface FontProp {
-    fontSize?: string;
-    fontWeight?: string;
-    fontColor?: string;
-    margin?: string;
+  fontSize?: string;
+  fontWeight?: string;
+  fontColor?: string;
+  margin?: string;
 }
 
 const ResultTextWrapper = styled.div`
-    width: 63%;
-    height: 100%;
-    background: #121212;
-    border-radius: 5px;
+  width: 63%;
+  height: 100%;
+  background: #121212;
+  border-radius: 5px;
+
+  @media ${device.tablet} {
+    width: 100%;
+    height: 600px;
+  }
 `;
 const Wrapper = styled.div`
-    padding: 50px;
-    box-sizing: border-box;
-    width: 100%;
-    height: 100%;
+  padding: 50px;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+
+  @media ${device.tablet} {
+    padding: 40px;
+    font-size: 13px;
+  }
 `;
 const TopWrapper = styled.div`
-    height: 80%;
+  height: 80%;
 `;
 const BottomWrapper = styled.div`
-    height: 20%;
-    display: flex;
-    flex-direction: column-reverse;
+  height: 20%;
+  display: flex;
+  flex-direction: column-reverse;
 `;
 
 const WhiteText = styled.div`
-    color: white;
-    font-size: ${(props: FontProp) => props.fontSize};
-    font-weight: ${(props: FontProp) => props.fontWeight};
-    margin: ${(props: FontProp) => (props.margin ? props.margin : "3px")};
+  color: white;
+  font-size: ${(props: FontProp) => props.fontSize};
+  font-weight: ${(props: FontProp) => props.fontWeight};
+  margin: ${(props: FontProp) => (props.margin ? props.margin : "3px 0px")};
 `;
 const MoneyText = styled.div`
-    color: #deddb6;
-    font-size: 5em;
-    font-weight: bold;
-    margin: 6px 0px 40px 0px;
+  color: #deddb6;
+  font-size: 5em;
+  font-weight: bold;
+  margin: 6px 0px 40px 0px;
+
+  @media ${device.tablet} {
+    font-size: 4.6em;
+  }
 `;
 const BoldText = styled.b`
-    font-weight: bold;
+  font-weight: bold;
 `;
 const CopyBox = styled.div`
-    cursor: pointer;
-    display: flex;
+  cursor: pointer;
+  display: flex;
 `;
 const CopyImg = styled.img`
-    margin: 3px;
+  margin: 3px;
 `;
 
 const ResultText = (): JSX.Element => {
-    const name = useSelector((state: any) => state.name);
-    const detail = "개발자";
+  const name = useSelector((state: any) => state.name);
+  const detail = "개발자";
 
-    const money: number = 30000;
-    const count: number = 1;
-    const copyBankAccount = () => {
-        var content: string = "국민은행 42400201291764 이의진";
+  const money: number = 30000;
+  const count: number = 1;
+  const copyBankAccount = () => {
+    var content: string = "국민은행 42400201291764 이의진";
 
-        navigator.clipboard.writeText(content).then(() => {
-            alert("복사되었습니다! 수고비 감사합니다~");
-        });
-    };
+    navigator.clipboard.writeText(content).then(() => {
+      alert("복사되었습니다! 수고비 감사합니다~");
+    });
+  };
 
-    const array = [
-        { price: 30000, userId: "" },
-        { price: 7000, userId: "" },
-        { price: 7000, userId: "" },
-        { price: 7000, userId: "" },
-        { price: 7000, userId: "" },
-        { price: 6000, userId: "" },
-        { price: 6000, userId: "" },
-        { price: 6000, userId: "" },
-        { price: 6000, userId: "" },
-        { price: 5000, userId: "" },
-        { price: 5000, userId: "" },
-        { price: 5000, userId: "" },
-        { price: 5000, userId: "" },
-        { price: 1000, userId: "" },
-    ];
+  const array = [
+    { price: 30000, userId: "" },
+    { price: 7000, userId: "" },
+    { price: 7000, userId: "" },
+    { price: 7000, userId: "" },
+    { price: 7000, userId: "" },
+    { price: 6000, userId: "" },
+    { price: 6000, userId: "" },
+    { price: 6000, userId: "" },
+    { price: 6000, userId: "" },
+    { price: 5000, userId: "" },
+    { price: 5000, userId: "" },
+    { price: 5000, userId: "" },
+    { price: 5000, userId: "" },
+    { price: 1000, userId: "" },
+  ];
 
-    // 아직 매칭되지 않은 가격 array를 랜덤으로 섞어 그 첫 번째 값
-    const price = array
-        .filter((ele) => !ele.userId || ele.userId.length === 0)
-        .sort(() => Math.random() - 0.5)[0];
+  // 아직 매칭되지 않은 가격 array를 랜덤으로 섞어 그 첫 번째 값
+  const price = array
+    .filter((ele) => !ele.userId || ele.userId.length === 0)
+    .sort(() => Math.random() - 0.5)[0];
 
-    return (
-        <ResultTextWrapper>
-            <Wrapper>
-                <TopWrapper>
-                    <WhiteText fontSize={"1.1em"}>{detail}</WhiteText>
-                    <WhiteText fontSize={"1.6em"} fontWeight={"600"}>
-                        {name}의 수고비
-                    </WhiteText>
-                    <MoneyText>{price.price.toLocaleString('ko-KR')}원</MoneyText>
-                    <WhiteText fontWeight={"100"}>
-                        달력 발주 비용 : 8,900원 X 수량 ({count})
-                    </WhiteText>
-                    <WhiteText fontWeight={"100"}>
-                        랜덤 수고비 : {price.price.toLocaleString('ko-KR')}원
-                    </WhiteText>
+  return (
+    <ResultTextWrapper>
+      <Wrapper>
+        <TopWrapper>
+          <WhiteText fontSize={"1.1em"}>{detail}</WhiteText>
+          <WhiteText fontSize={"1.6em"} fontWeight={"600"}>
+            {name}의 수고비
+          </WhiteText>
+          <MoneyText>{price.price.toLocaleString("ko-KR")}원</MoneyText>
+          <WhiteText fontWeight={"100"}>
+            달력 발주 비용 : 8,900원 X 수량 ({count})
+          </WhiteText>
+          <WhiteText fontWeight={"100"}>
+            랜덤 수고비 : {price.price.toLocaleString("ko-KR")}원
+          </WhiteText>
 
-                    <WhiteText
-                        fontSize={"1.3em"}
-                        fontWeight={"300"}
-                        margin={"5px 0px"}
-                    >
-                        총 입금 금액:{" "}
-                        <BoldText>{(price.price + 8900 * count).toLocaleString('ko-KR')}원</BoldText>
-                    </WhiteText>
-                </TopWrapper>
-                <BottomWrapper>
-                    <WhiteText fontWeight={"100"}>
-                        카카오뱅크도 가능합니다.
-                    </WhiteText>
-                    <CopyBox >
-                        <WhiteText fontWeight={"100"}>
-                            국민은행 42400201291764 이의진
-                        </WhiteText>
-                        <CopyImg src={imageSrc} onClick={copyBankAccount}/>
-                    </CopyBox>
-                </BottomWrapper>
-            </Wrapper>
-        </ResultTextWrapper>
-    );
+          <WhiteText fontSize={"1.3em"} fontWeight={"300"} margin={"5px 0px"}>
+            총 입금 금액:{" "}
+            <BoldText>
+              {(price.price + 8900 * count).toLocaleString("ko-KR")}원
+            </BoldText>
+          </WhiteText>
+        </TopWrapper>
+        <BottomWrapper>
+          <WhiteText fontWeight={"100"}>카카오뱅크도 가능합니다.</WhiteText>
+          <CopyBox>
+            <WhiteText fontWeight={"100"}>
+              국민은행 42400201291764 이의진
+            </WhiteText>
+            <CopyImg src={imageSrc} onClick={copyBankAccount} />
+          </CopyBox>
+        </BottomWrapper>
+      </Wrapper>
+    </ResultTextWrapper>
+  );
 };
 
 export default ResultText;
--- random-donation/src/page/Result.tsx
+++ random-donation/src/page/Result.tsx
@@ -4,48 +4,97 @@ import FeedBack from "../component/Result/FeedBack";
 import RankList from "../component/Result/RankList";
 import ResultText from "../component/Result/ResultText";
 
-import { device } from "../services/devices";
+import { device, min_device } from "../services/devices";
 import thankYou from "../assets/thanks.png";
 
 const Container = styled.div`
   width: 100%;
-  height: 100vh;
-  overflow: hidden;
   font-size: 15px;
+
+  @media ${min_device.tablet} {
+    overflow: hidden;
+    height: 100vh;
+  }
+  @media ${device.tablet} {
+    font-size: 14px;
+  }
 `;
 const ResultWrapper = styled.div`
   width: 940px;
   height: 100%;
   margin: 0 auto;
-  padding: 20px;
+
+  @media ${min_device.tablet} {
+    padding: 20px;
+  }
+  @media ${device.tablet} {
+    width: 90%;
+    margin: 30px auto;
+  }
 `;
 const Top = styled.div`
   width: 100%;
   height: 50%;
   display: flex;
   justify-content: space-between;
+
+  @media ${device.tablet} {
+    display: block;
+  }
 `;
 const Bottom = styled.div`
-  width: 100%;
-  height: 90%;
-  margin: 2% 0;
+  @media ${min_device.tablet} {
+    margin: 2% 0;
+    width: 100%;
+    height: 90%;
+  }
+  @media ${device.tablet} {
+    margin-top: 25px;
+  }
 `;
 const RightContent = styled.div`
   width: 35%;
+
+  @media ${device.tablet} {
+    width: 100%;
+    margin-top: 25px;
+  }
 `;
 const BannerContent = styled.div`
-  height: 35%;
-  display: flex;
-  flex-direction: column-reverse;
+  @media ${min_device.tablet} {
+    height: 35%;
+    display: flex;
+    flex-direction: column-reverse;
+  }
 `;
-const Banner = styled.div`
+const Banner = styled.img`
   width: 100%;
-  height: 90%;
-  background-image: url("${thankYou}");
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  border-radius: 5px;
+  cursor: pointer;
+  @media ${device.tablet} {
+    margin-top: 25px;
+    height: 370px;
+  }
+  @media (max-width: 750px) {
+    height: 340px;
+  }
+  @media (max-width: 630px) {
+    height: 300px;
+  }
+  @media (max-width: 570px) {
+    height: 280px;
+  }
+  @media (max-width: 540px) {
+    height: 240px;
+  }
+  @media ${device.mobileL} {
+    height: 220px;
+  }
+  @media (max-width: 400px) {
+    height: 200px;
+  }
+  @media ${device.mobileM} {
+    height: 170px;
+  }
 `;
 
 const Result = (): JSX.Element => {
@@ -57,7 +106,7 @@ const Result = (): JSX.Element => {
           <RightContent>
             <RankList />
             <BannerContent>
-              <Banner />
+              <Banner src={thankYou} />
             </BannerContent>
           </RightContent>
         </Top>
