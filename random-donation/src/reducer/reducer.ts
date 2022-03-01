7aace8175135f0669abeff24225630f95933f73c
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-03-01 19:41:28 +0900

    [Feat] 결과 페이지 firebase 연결

--- random-donation/src/reducer/reducer.ts
+++ random-donation/src/reducer/reducer.ts
@@ -1,46 +1,75 @@
 import { createAction, handleActions } from "redux-actions";
 import firestore from "../services/fbase";
+import UserData from "./types";
 
 // action type
 const USERS_LIST = "USERS_LIST";
 const USER_SELECT = "USER_SELECT";
+const USER_RANK_LIST = "USER_RANK_LIST"
 
+export const user_rank_list = createAction(USER_RANK_LIST);
 export const users_list = createAction(USERS_LIST);
 export const user_select = createAction(USER_SELECT);
 
 export const firebase_user_list = () => {
-  return (dispatch: any) => {
-    return firestore
-      .collection("users")
-      .get()
-      .then((snapshot: any) => {
-        var rows: any = [];
-        snapshot.forEach((doc: any) => {
-          var childData = doc.data();
-          rows.push(childData);
-        });
-        dispatch(users_list(rows));
-      });
-  };
+    return (dispatch: any) => {
+        return firestore
+            .collection("users")
+            .get()
+            .then((snapshot: any) => {
+                var rows: any = [];
+
+                snapshot.forEach((doc: any) => {
+                    var childData = doc.data();
+                    rows.push(childData);
+                });
+
+                dispatch(users_list(rows));
+            });
+    };
+};
+
+export const firebase_user_rank_list = () => {
+    return (dispatch: any) => {
+        return firestore
+            .collection("users")
+            .where("isConfirm", "==", true)
+            .orderBy("result", "desc")
+            .limit(5)
+            .get()
+            .then((snapshot: any) => {
+                var rows: any = [];
+
+                snapshot.forEach((doc: any) => {
+                    var childData = doc.data();
+                    rows.push(childData);
+                });
+
+                dispatch(user_rank_list(rows));
+            });
+    };
 };
 
 const initialState = {
-  users: {},
-  selectedUser: {},
+    users: {},
+    selectedUser: {},
 };
 
 export default handleActions(
-  {
-    [USERS_LIST]: (state: any, data: any) => {
-      return { users: data, selectedUser: {} };
+    {
+        [USERS_LIST]: (state: any, data: any) => {
+            return { users: data, selectedUser: {} };
+        },
+        [USER_SELECT]: (state: any, name: any) => {
+            let users = state.users;
+            return {
+                users: users,
+                selectedUser: users.find((row: any) => row.name === name),
+            };
+        },
+        [USER_RANK_LIST]: (state: any, data: any)=> {
+          return {users: data || data.filter((ele: UserData) => ele.isConfirm).slice(0, 5)}
+        }
     },
-    [USER_SELECT]: (state: any, name: any) => {
-      let users = state.users;
-      return {
-        users: users,
-        selectedUser: users.find((row: any) => row.name === name),
-      };
-    },
-  },
-  initialState
+    initialState
 );
---
+++ random-donation/src/reducer/types.ts
@@ -0,0 +1,8 @@
+export default interface UserData {
+    name: string;
+    detail: string;
+    isConfirm: boolean;
+    count: number;
+    result: number;
+    isAdmin?: boolean;
+}
