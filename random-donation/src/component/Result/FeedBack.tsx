1a9e74bebf729cdbebbfc73a5101be324bb4ef94
areumsheep <areumsheep@gmail.com>
2022-03-01 20:59:09 +0900

    [Feat] 결과 페이지 디자인 수정

--- random-donation/src/component/Result/FeedBack.tsx
+++ random-donation/src/component/Result/FeedBack.tsx
@@ -1,122 +1,105 @@
 import React from "react";
 import styled from "styled-components";
-import { device } from '../../services/devices';
+import { device } from "../../services/devices";
 
 interface FontProp {
-    fontSize: string;
-    fontWeight: string;
-    fontColor: string;
-    marginTop?: string | null;
-    padding?: string;
-    paddingLeft?: string;
+  fontSize: string;
+  fontWeight: string;
+  fontColor: string;
+  marginTop?: string | null;
+  padding?: string;
+  paddingLeft?: string;
 }
 
 const BottomBox = styled.div`
-    position: relative;
-    width: 65%;
-    height: 45vh;
-    background: #f6f6f6;
-    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 5px;
+  position: relative;
+  width: 65%;
+  height: 45vh;
+  background: #f6f6f6;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
 
-    margin-top: 1.2rem;
-    font-family: Noto Sans KR;
+  margin-top: 1.2rem;
+  font-family: Noto Sans KR;
 
-    @media ${device.tablet} {
-        height: 25vh;
-        margin-top: 0;
-        margin-bottom: 50px;
-    }
+  @media ${device.tablet} {
+    height: 25vh;
+    margin-top: 0;
+    margin-bottom: 50px;
+  }
 `;
 
 const Text = styled.p`
-    font-size: ${(props: FontProp) => props.fontSize};
-    font-weight: ${(props: FontProp) => props.fontWeight};
-    color: ${(props: FontProp) => props.fontColor};
-    
-    @media ${device.mobileL || device.tablet} {
-        font-size: calc(${(props: FontProp) => props.fontSize} / 2 + 5px);
-    }
+  font-size: ${(props: FontProp) => props.fontSize};
+  font-weight: ${(props: FontProp) => props.fontWeight};
+  color: ${(props: FontProp) => props.fontColor};
 
-    padding: 5px;
+  @media ${device.mobileL || device.tablet} {
+    font-size: calc(${(props: FontProp) => props.fontSize} / 2 + 5px);
+  }
+
+  padding: 5px;
 `;
 
 const TextBox = styled.div`
-    margin-top: 20px;
-    margin-left: 30px;
+  margin-top: 20px;
+  margin-left: 30px;
 `;
 
 const Form = styled.textarea`
-    width: 80%;
-    height: 100px;
+  width: 80%;
+  height: 100px;
 
-    margin-top: 20px;
-    margin-left: 32px;
-    background-color: transparent;
-    border: none;
-    outline: none;
+  margin-top: 20px;
+  margin-left: 32px;
+  background-color: transparent;
+  border: none;
+  outline: none;
 
-    font-size: 1.2rem;
-    font-family: Noto Sans KR;
+  font-size: 1.2rem;
+  font-family: Noto Sans KR;
 
-    @media ${device.desktop} {
-        height: 75%;
-    }
+  @media ${device.desktop} {
+    height: 75%;
+  }
 
-    @media ${device.tablet} {
-        width: 75%;
-        height: 30%;
-    }
-
+  @media ${device.tablet} {
+    width: 75%;
+    height: 30%;
+  }
 `;
 
 const Button = styled.button`
-    position: absolute;
-    right: 2rem;
-    bottom: 2rem;
+  position: absolute;
+  right: 2rem;
+  bottom: 2rem;
 
-    font-family: Noto Sans KR;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 1rem;
+  font-family: Noto Sans KR;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 1rem;
 
-    display: flex;
-    align-items: center;
-    text-align: center;
+  display: flex;
+  align-items: center;
+  text-align: center;
 
-    color: #ffffff;
-    background: #121212;
-    border: 1px solid #fafafa;
-    box-sizing: border-box;
-    border-radius: 3px;
+  color: #ffffff;
+  background: #121212;
+  border: 1px solid #fafafa;
+  box-sizing: border-box;
+  border-radius: 3px;
 
-    padding: 0.5rem;
+  padding: 0.5rem;
+`;
+
+const FeedBackWrapper = styled.div`
+  width: 100%;
+  height: 48%;
+  background: #5485;
 `;
 
 const FeedBack = (): JSX.Element => {
-    return (
-        <BottomBox>
-            <TextBox>
-                <Text
-                    fontSize={"1.8rem"}
-                    fontWeight={"900"}
-                    fontColor={"#121212"}
-                >
-                    2022 달력 피드백
-                </Text>
-                <Text
-                    fontSize={"1.2rem"}
-                    fontWeight={"normal"}
-                    fontColor={"#121212"}
-                >
-                    앞으로 달력하면서 하고 싶은 것을 써주삼요 헛소리 써줘도
-                    감사히 읽음
-                </Text>
-            </TextBox>
-            <Form autoFocus={true}></Form>
-            <Button>Send!</Button>
-        </BottomBox>
-    );
+  return <FeedBackWrapper>sadkfj</FeedBackWrapper>;
 };
 
 export default FeedBack;
--- random-donation/src/component/Result/ResultText.tsx
+++ random-donation/src/component/Result/ResultText.tsx
@@ -1,168 +1,102 @@
 import React from "react";
 import styled from "styled-components";
-import { device } from '../../services/devices';
-import imageSrc from '../../assets/image.png';
+import { device } from "../../services/devices";
+import imageSrc from "../../assets/image.png";
 
 interface FontProp {
-    fontSize: string;
-    fontWeight: string;
-    fontColor: string;
-    marginTop?: string | null;
+  fontSize?: string;
+  fontWeight?: string;
+  fontColor?: string;
+  margin?: string;
 }
 
-interface BoxProp {
-    padding: string;
-}
-
-const Container = styled.div`
-    position: relative;
-
-    width: 60%;
-    height: 100%;
-    background: #121212;
-    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 5px;
-
-    color: #ffffff;
-    font-family: Noto Sans KR;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 17px;
-    line-height: 25px;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    overflow: hidden;
-
-    @media ${device.tablet} {
-        width: 80%;
-        height: 30vh;   
-    }
+const ResultTextWrapper = styled.div`
+  width: 63%;
+  height: 100%;
+  background: #121212;
+  border-radius: 5px;
+`;
+const Wrapper = styled.div`
+  padding: 50px;
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+`;
+const TopWrapper = styled.div`
+  height: 80%;
+`;
+const BottomWrapper = styled.div`
+  height: 20%;
+  display: flex;
+  flex-direction: column-reverse;
 `;
 
-const ICON = styled.div`
-    position: absolute;
-    bottom: -20px;
-    right: 20px;
-
-    @media ${device.tablet} {
-        display: none;
-    }
+const WhiteText = styled.div`
+  color: white;
+  font-size: ${(props: FontProp) => props.fontSize};
+  font-weight: ${(props: FontProp) => props.fontWeight};
+  margin: ${(props: FontProp) => (props.margin ? props.margin : "3px")};
 `;
-
-const Text = styled.p`
-    font-size: ${(props: FontProp) => props.fontSize};
-    font-weight: ${(props: FontProp) => props.fontWeight};
-    padding: 5px;
-    margin-top: ${(props: FontProp) => props.marginTop};
-    color: ${(props: FontProp) => props.fontColor};
+const MoneyText = styled.div`
+  color: #deddb6;
+  font-size: 5em;
+  font-weight: bold;
+  margin: 6px 0px 40px 0px;
 `;
-
-const Box = styled.div`
-    padding: ${(props: BoxProp) => props.padding};
+const BoldText = styled.b`
+  font-weight: bold;
 `;
-
-const Icon2 = styled.img``;
-
-const Icon = (): JSX.Element => {
-    return (
-        <ICON>
-            <svg
-                width="126"
-                height="72"
-                viewBox="0 0 126 72"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    d="M124 20C124 21.8431 122.821 23.9542 119.849 26.1853C116.911 28.3904 112.532 30.4613 106.943 32.2359C95.7861 35.7776 80.2498 38 63 38C45.7502 38 30.2139 35.7776 19.0574 32.2359C13.4676 30.4613 9.08938 28.3904 6.15143 26.1853C3.17878 23.9542 2 21.8431 2 20C2 18.1569 3.17878 16.0458 6.15143 13.8147C9.08938 11.6096 13.4676 9.53865 19.0574 7.76411C30.2139 4.22239 45.7502 2 63 2C80.2498 2 95.7861 4.22239 106.943 7.76411C112.532 9.53865 116.911 11.6096 119.849 13.8147C122.821 16.0458 124 18.1569 124 20Z"
-                    stroke="white"
-                    strokeWidth="4"
-                />
-                <path
-                    d="M124 50C124 61.0457 96.6894 70 63 70C29.3106 70 2 61.0457 2 50"
-                    stroke="white"
-                    strokeWidth="4"
-                />
-                <line
-                    x1="2"
-                    y1="20"
-                    x2="2"
-                    y2="51"
-                    stroke="white"
-                    strokeWidth="4"
-                />
-                <line
-                    x1="124"
-                    y1="20"
-                    x2="124"
-                    y2="51"
-                    stroke="white"
-                    strokeWidth="4"
-                />
-            </svg>
-        </ICON>
-    );
-};
+const CopyBox = styled.div`
+  cursor: pointer;
+  display: flex;
+`;
+const CopyImg = styled.img`
+  margin: 3px;
+`;
 
 const ResultText = (): JSX.Element => {
-    const copyBankAccount = () =>{
-        var content:string = "국민은행 42400201291764 이의진"
+  const detail = "개발자";
+  const name = "양아름";
+  const money: number = 30000;
+  const count: number = 1;
+  const copyBankAccount = () => {
+    var content: string = "국민은행 42400201291764 이의진";
 
-        navigator.clipboard.writeText(content)
-        .then(() => {
-            alert('복사 되었습니다!')
-        })
-    };
+    navigator.clipboard.writeText(content).then(() => {
+      alert("복사되었습니다! 수고비 감사합니다~");
+    });
+  };
 
-    return (
-        <Container>
-            <Box padding={"30px"}>
-                <Text
-                    fontSize={"1.2rem"}
-                    fontWeight={"700"}
-                    fontColor={"#ffffff"}
-                >
-                    개발자 양아름의 수고비 :{" "}
-                </Text>
-                <Text
-                    fontSize={"2.5rem"}
-                    fontWeight={"900"}
-                    fontColor={"#DEDDB6"}
-                >
-                    30,000원
-                </Text>
+  return (
+    <ResultTextWrapper>
+      <Wrapper>
+        <TopWrapper>
+          <WhiteText fontSize={"1.1em"}>{detail}</WhiteText>
+          <WhiteText fontSize={"1.6em"} fontWeight={"600"}>
+            {name}의 수고비
+          </WhiteText>
+          <MoneyText>{money}원</MoneyText>
+          <WhiteText fontWeight={"100"}>
+            달력 발주 비용 : 8,900원 X 수량 ({count})
+          </WhiteText>
+          <WhiteText fontWeight={"100"}>랜덤 수고비 : {money}원</WhiteText>
 
-                <Text
-                    fontSize={"1rem"}
-                    fontWeight={"400"}
-                    fontColor={"#ffffff"}
-                    marginTop={"20px"}
-                >
-                    {" "}
-                    달력 발주 비용 : 8,900원 X 수량 (1)
-                    <br />
-                    랜덤 수고비 : 30,000원
-                    <br />
-                    총 입금 금액 : 38,900원
-                    <br />
-                </Text>
-            </Box>
-
-            <Box padding={"30px"}>
-                <Text
-                    fontSize={"0.8rem"}
-                    fontWeight={"300"}
-                    fontColor={"#ffffff"}
-                >
-                    국민은행 42400201291764 이의진{" "}
-                    <Icon2 src={imageSrc} onClick={copyBankAccount}/>
-                </Text>
-            </Box>
-            <Icon />
-        </Container>
-    );
+          <WhiteText fontSize={"1.3em"} fontWeight={"300"} margin={"5px 0px"}>
+            총 입금 금액: <BoldText>{money + 8900 * count}원</BoldText>
+          </WhiteText>
+        </TopWrapper>
+        <BottomWrapper>
+          <WhiteText fontWeight={"100"}>카카오뱅크도 가능합니다.</WhiteText>
+          <CopyBox onClick={copyBankAccount}>
+            <WhiteText fontWeight={"100"}>
+              국민은행 42400201291764 이의진
+            </WhiteText>
+            <CopyImg src={imageSrc} />
+          </CopyBox>
+        </BottomWrapper>
+      </Wrapper>
+    </ResultTextWrapper>
+  );
 };
 
 export default ResultText;
