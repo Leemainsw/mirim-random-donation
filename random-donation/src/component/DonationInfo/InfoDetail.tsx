64c0e48248ec6eeb7f254c5dc7a9d304c6c53246
areumsheep <areumsheep@gmail.com>
2022-03-01 17:24:26 +0900

    [Add] 메인 페이지 추가

--- random-donation/src/component/DonationInfo/InfoDetail.tsx
+++ random-donation/src/component/DonationInfo/InfoDetail.tsx
@@ -1,12 +1,66 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { connect } from "react-redux";
+import TeacherGift from "../../assets/Teacher_Gift.jpg";
 import { firebase_user_list } from "../../reducer/reducer";
 
-const DetailWrapper = styled.div`
+const InfoDetailWrapper = styled.div`
   width: 45%;
   height: 100%;
   background: #ffffff;
+`;
+const DetailWrapper = styled.div`
+  width: 80%;
+  height: 90%;
+  margin: 50px auto;
+  // background: #d3d3d3;
+  overflow-y: auto;
+`;
+const Border = styled.div`
+  width: 100%;
+  height: 30px;
+`;
+
+const TitleWord = styled.h1`
+  color: #131313;
+  font-size: 1.8em;
+  font-weight: bold;
+`;
+const DetailWordWrapper = styled.div`
+  width: 90%;
+  margin: 16px auto;
+`;
+const DetailWord = styled.div`
+  color: #131313;
+  font-size: 1em;
+  margin: 7px 0px;
+`;
+const Image = styled.img`
+  width: 100%;
+  margin: 5px 0px 20px 0px;
+`;
+const BoldWord = styled.b`
+  font-weight: bold;
+`;
+const InputName = styled.input`
+  border: 0px;
+  outline: 0px;
+  background: #f9f9f9;
+  padding-left: 10px;
+  height: 35px;
+  width: 300px;
+  margin: 10px 0px;
+`;
+const GoButton = styled.button`
+  border: 0px;
+  outline: 0px;
+  width: 300px;
+  height: 35px;
+  background: #121212;
+  color: white;
+  border-radius: 3px;
+  font-weight: bold;
+  text-align: center;
 `;
 
 const InfoDetail = (props: any) => {
@@ -17,9 +71,66 @@ const InfoDetail = (props: any) => {
   //   console.log(users);
   // }, []);
   return (
-    <DetailWrapper>
-      <h1>hello</h1>
-    </DetailWrapper>
+    <InfoDetailWrapper>
+      <DetailWrapper>
+        <TitleWord>정산방법</TitleWord>
+        <DetailWordWrapper>
+          <DetailWord>1. 달력은 개당 8900원이다.</DetailWord>
+          <DetailWord>2. 8900원에 너가 산 달력 개수를 곱한다.</DetailWord>
+          <DetailWord>3. 거기에 선생님들 달력 값 1900원을 더한다.</DetailWord>
+          <DetailWord>4. 거기에 랜덤으로 정해진 수고비를 더한다.</DetailWord>
+          <DetailWord>5. 끝!</DetailWord>
+        </DetailWordWrapper>
+
+        <Border />
+
+        <TitleWord>도네이션 확률</TitleWord>
+        <DetailWordWrapper>
+          <DetailWord>3만원 : 1명</DetailWord>
+          <DetailWord>7천원 : 4명</DetailWord>
+          <DetailWord>6천원 : 4명</DetailWord>
+          <DetailWord>5천원 : 4명</DetailWord>
+          <DetailWord>천원 : 1명</DetailWord>
+        </DetailWordWrapper>
+
+        <Border />
+
+        <TitleWord>알아두면 좋은 정보</TitleWord>
+        <DetailWordWrapper>
+          <DetailWord>
+            - 3만원 도네이션 당첨되면 애장품 증정 받을 수 있는 영광이 주어짐.
+          </DetailWord>
+          <DetailWord>- 3만원 도네이션 당첨되면 추가 식사권 증정.</DetailWord>
+          <DetailWord>- 다른 사람 사칭한 거 들키면 벌칙 있습니다.</DetailWord>
+          <DetailWord>- 수고비 더 주고싶으면 더 줘도 됨.</DetailWord>
+        </DetailWordWrapper>
+
+        <Border />
+
+        <TitleWord>FAQ</TitleWord>
+        <DetailWordWrapper>
+          <DetailWord>Q. 선생님들 달력은 잘 드렸나요?</DetailWord>
+          <DetailWord>
+            A. 예. 박성래 선생님이 잘 받았다고 하셨습니다.
+          </DetailWord>
+          <Image src={TeacherGift} />
+          <DetailWord>Q. 도네이션 금액은 어떻게 정하는 건가요?</DetailWord>
+          <DetailWord>
+            A. 밑에 체크박스에 <BoldWord>이름을 적으면 달력의 요정</BoldWord>이
+            도네 금액을 정해줄 것임요.
+          </DetailWord>
+        </DetailWordWrapper>
+
+        <Border />
+
+        <TitleWord>수고비 보러가기</TitleWord>
+        <DetailWordWrapper>
+          <InputName placeholder="이름 세글자 써주라 이상한 거 쓰지마라 진짜" />
+          <br />
+          <GoButton>달력의 요정아! 도와줘!</GoButton>
+        </DetailWordWrapper>
+      </DetailWrapper>
+    </InfoDetailWrapper>
   );
 };
 
--- random-donation/src/component/DonationInfo/InfoTitle.tsx
+++ random-donation/src/component/DonationInfo/InfoTitle.tsx
@@ -1,23 +1,58 @@
 import React from "react";
 import styled from "styled-components";
 
-const TitleWrapper = styled.div`
+const InfoTitleWrapper = styled.div`
   width: 55%;
   height: 100%;
   background: #121212;
 `;
+const TitleWrapper = styled.div`
+  padding: 50px 70px;
+`;
 const ThinWhiteTitle = styled.h1`
   color: white;
   font-weight: 100;
-  font-size: 80px;
+  font-size: 6.8em;
+`;
+const BoldYellowTitle = styled.h1`
+  color: #deddb6;
+  font-weight: bold;
+  font-size: 6.8em;
+`;
+const BorderBottom = styled.div`
+  margin: 30px 0px;
+  width: 60%;
+  height: 4px;
+  background: white;
+`;
+const DetailWord = styled.p`
+  color: white;
+  font-weight: 100;
+  font-size: 1.1em;
+  margin: 1px 0px;
+`;
+const BoldWord = styled.b`
+  font-weight: bold;
 `;
 
 const InfoTitle = () => {
   return (
-    <TitleWrapper>
-      <ThinWhiteTitle>2022달력</ThinWhiteTitle>
-      <ThinWhiteTitle>수고비</ThinWhiteTitle>
-    </TitleWrapper>
+    <InfoTitleWrapper>
+      <TitleWrapper>
+        <ThinWhiteTitle>2022달력</ThinWhiteTitle>
+        <ThinWhiteTitle>수고비</ThinWhiteTitle>
+        <BoldYellowTitle>랜덤</BoldYellowTitle>
+        <BoldYellowTitle>도네이션!</BoldYellowTitle>
+        <BorderBottom />
+        <DetailWord>
+          성황리에 끝난 2022달력! 이제 마지막 할 일 정산시간이
+        </DetailWord>
+        <DetailWord>
+          당신을 기다리고 있다! 근데 이제 <BoldWord>랜덤 도네이션</BoldWord>을
+          곁들인...
+        </DetailWord>
+      </TitleWrapper>
+    </InfoTitleWrapper>
   );
 };
 
